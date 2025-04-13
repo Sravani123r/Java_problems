5ecde98daa97107f02cf9b21236818f6e98e0afa
Sravani123r <bsreddy3020@gmail.com>
2025-04-13 23:14:43 +0530

    problems

---
+++ src/Recursion/NaturalNumbers.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class NaturalNumbers {
+	static void printNatural(int n) {
+		if(n==0) {
+			return;
+		}
+		printNatural(n-1);
+		System.out.print(n+" ");
+	}
+	
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		printNatural(n);
+	}
+}
